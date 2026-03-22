629823b79055fd4a0c5f3faa4d9065fb67cc5331
Erin Cumiskey <ecumiskey00@gmail.com>
2026-03-22 11:14:13 +0000

    Small update

--- Warren/Assets/Scripts/Managers/GameManager.cs
+++ Warren/Assets/Scripts/Managers/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    bool toDestroy;
 
     private void Awake()
     {
         if (Instance == null) { Instance = this; }
-        else { Destroy(gameObject); }
+        else { Destroy(gameObject); toDestroy = true; }
+
         DontDestroyOnLoad(gameObject);
 
         SceneManager.sceneLoaded += SceneLoad;
@@ -37,12 +39,16 @@ public class GameManager : MonoBehaviour
 
     private void SceneLoad(Scene arg0, LoadSceneMode arg1)
     {
+        if (toDestroy) return;
+
         SaveLoad.Load();
         Debug.Log("Scene loaded " + arg0.name);
     }
 
     private void SceneUnloaded(Scene arg0)
     {
+        if (toDestroy) return;
+
         SaveLoad.Save();        
     }
 
